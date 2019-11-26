5a9309948f69467c85d9c7163dd4e42467a4a3db
Victor Kurinny <tyokman@gmail.com>
2019-11-26 10:48:39 +0200

    AudioStreamerFramework

---
+++ AudioStreamerFramework/AudioStreamerFramework/AudioStreamerFramework.h
@@ -0,0 +1,19 @@
+//
+//  AudioStreamerFramework.h
+//  AudioStreamerFramework
+//
+//  Created by Victor Kurinny on 11/26/19.
+//  Copyright © 2019 BeatHead. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioStreamerFramework.
+FOUNDATION_EXPORT double AudioStreamerFrameworkVersionNumber;
+
+//! Project version string for AudioStreamerFramework.
+FOUNDATION_EXPORT const unsigned char AudioStreamerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioStreamerFramework/PublicHeader.h>
+
+
